ba01bd412aa2cc55539f1eb11650492716fd55ba
Joshua-Production <joshuaespinosabusiness@outlook.com>
2024-12-02 16:59:22 -0600

    Add project files.

---
+++ AIE_01/AIE_01.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	const int NUM_NUMBERS = 5;
+	int numbers[NUM_NUMBERS] = { 10, 20, 30, 40, 50 };
+
+	/* 
+	TODO: Print each value of the array in order
+	Expected:
+		10, 20, 30, 40, 50,
+	*/
+	// Task1: Write your Code here
+	// ------------------------------------------------------------------------
+	for (int i = 0; i < NUM_NUMBERS; i++)
+	{
+		std::cout << numbers[i] << std::endl;
+	}
+	std::cout << "" << std::endl;
+	// ------------------------------------------------------------------------
+
+
+	/*
+	TODO: Print each value of the array in reverse order
+	Expected:
+		50, 40, 30, 20, 10,
+	*/
+
+	// Task 2: Write your Code here
+	// ------------------------------------------------------------------------
+	for (int i = NUM_NUMBERS -1; i >= 0; i--)
+	{
+		std::cout << numbers[i] << std::endl;
+	}
+
+	// ------------------------------------------------------------------------
+
+	return 0;
+}
